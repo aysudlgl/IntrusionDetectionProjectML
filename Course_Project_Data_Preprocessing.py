d547f741b6e95748739ac2dcd5a82703dc7d4661
wtenetra <147451974+wtenetra@users.noreply.github.com>
2024-07-30 05:12:31 -0500

    Update Course_Project_Data_Preprocessing.py

--- Course_Project_Data_Preprocessing.py
+++ Course_Project_Data_Preprocessing.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from keras.callbacks import EarlyStopping
 from keras.callbacks import ModelCheckpoint
 
-#Load the dataset
+#Add the columns of the dataset
 
 column_names = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 
     'dst_bytes', 'land', 'wrong_fragment', 'urgent', 'hot', 'num_failed_logins', 
@@ -23,159 +23,179 @@ column_names = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes',
     'dst_host_srv_diff_host_rate', 'dst_host_serror_rate', 'dst_host_srv_serror_rate', 
     'dst_host_rerror_rate', 'dst_host_srv_rerror_rate', 'label']
 
-train = pd.read_csv("KDDTrain.csv", header=None, names = column_names)
-test = pd.read_csv("KDDTest.csv", header=None, names = column_names)
+#Load the datasets
+df = pd.read_csv("KDDTrain.csv", header=None, names = column_names)
+df_test = pd.read_csv("KDDTest.csv", header=None, names = column_names)
 
-print('Training set: ', train.shape)
-print('Test set: ', test.shape)
+print('Dimensions of the Training set: ', df.shape)
+print('Dimensions of the Test set: ', df_test.shape)
 
-train.head(5)
+df.head(5)
 
 #Explore dataset
 print('Label Count for Training Set: ')
-print(train['label'].value_counts())
+print(df['label'].value_counts())
 print()
 print('Label Count for Test Set: ')
-print(test ['label'].value_counts())
+print(df_test['label'].value_counts())
+
+#columns are categorical, not yet binary
 
 print('Training set:')
-for column_names in train.columns:
-    if train[column_names].dtypes == 'object' :
-        unique_cat = len(train[column_names].unique())
+for column_names in df.columns:
+    if df[column_names].dtypes == 'object' :
+        unique_cat = len(df[column_names].unique())
         print("Feature '{column_names}' has {unique_cat} categories".format(column_names=column_names, unique_cat=unique_cat))
 
 print()
 print('Label distribution: ')
-print(train['service'].value_counts().sort_values(ascending=False).head())
+print(df['service'].value_counts().sort_values(ascending=False).head())
 
 
+
+#Test set
 print('Test set:')
-for column_names in test.columns:
-    if test[column_names].dtypes == 'object' :
-        unique_cat = len(test[column_names].unique())
+for column_names in df_test.columns:
+    if df_test[column_names].dtypes == 'object' :
+        unique_cat = len(df_test[column_names].unique())
         print("Feature '{column_names}' has {unique_cat} categories".format(column_names=column_names, unique_cat=unique_cat))
 
 print()
 print('Label distribution: ')
-print(train['service'].value_counts().sort_values(ascending=False).head())
+print(df_test['service'].value_counts().sort_values(ascending=False).head())
 
-##ONE HOT ENCODING
+#LabelEncoder Insert categorical features into a 2D numpy array
+
 categorical_columns=['protocol_type', 'service', 'flag']
 
-train_categorical_values = train[categorical_columns]
-test_categorical_values = test[categorical_columns]
+df_categorical_values = df[categorical_columns]
+testdf_categorical_values = df_test[categorical_columns]
 
-train_categorical_values.head()
+df_categorical_values.head()
 
 
-#protocol type
-unique_protocol=sorted(train.protocol_type.unique())
+#Had to keep for One-Hot Encoding
+
+# protocol type
+unique_protocol=sorted(df.protocol_type.unique())
 string1 = 'Protocol_type_'
 unique_protocol2=[string1 + x for x in unique_protocol]
-print(unique_protocol2)
+#print(unique_protocol2)
 
-#service
-unique_service=sorted(train.service.unique())
+# service
+unique_service=sorted(df.service.unique())
 string2 = 'service_'
 unique_service2=[string2 + x for x in unique_service]
-print(unique_service2)
+#print(unique_service2)
 
-#flag
-unique_flag=sorted(train.flag.unique())
+
+# flag
+unique_flag=sorted(df.flag.unique())
 string3 = 'flag_'
 unique_flag2=[string3 + x for x in unique_flag]
-print(unique_flag2)
+#print(unique_flag2)
 
-#put together
+
+# put together
 dumcols=unique_protocol2 + unique_service2 + unique_flag2
 
-#test set
-unique_service_test=sorted(test.service.unique())
+
+#do it for test set
+unique_service_test=sorted(df_test.service.unique())
 unique_service2_test=[string2 + x for x in unique_service_test]
 testdumcols=unique_protocol2 + unique_service2_test + unique_flag2
 
 
-train_categorical_values_enc=train_categorical_values.apply(LabelEncoder().fit_transform)
+#Transform categorical features into numberes using LabelEncoder()
+df_categorical_values_enc=df_categorical_values.apply(LabelEncoder().fit_transform)
 
-print(train_categorical_values.head())
-print('\n')
-print(train_categorical_values_enc.head())
+print(df_categorical_values.head())
+print('--------------------')
+print(df_categorical_values_enc.head())
 
 # test set
-test_categorical_values_enc=test_categorical_values.apply(LabelEncoder().fit_transform)
+testdf_categorical_values_enc=testdf_categorical_values.apply(LabelEncoder().fit_transform)
 
+
+#One-Hot Encoding
 
 encoder = OneHotEncoder(categories='auto')
-train_categorical_values_encenc = encoder.fit_transform(train_categorical_values_enc)
-train_cat_data = pd.DataFrame(train_categorical_values_encenc.toarray(),columns=dumcols)
+df_categorical_values_encenc = encoder.fit_transform(df_categorical_values_enc)
+df_cat_data = pd.DataFrame(df_categorical_values_encenc.toarray(),columns=dumcols)
 
 
 # test set
-test_categorical_values_encenc = encoder.fit_transform(test_categorical_values_enc)
-test_cat_data = pd.DataFrame(test_categorical_values_encenc.toarray(),columns=testdumcols)
+testdf_categorical_values_encenc = encoder.fit_transform(testdf_categorical_values_enc)
+testdf_cat_data = pd.DataFrame(testdf_categorical_values_encenc.toarray(),columns=testdumcols)
 
-test_cat_data.head()
+df_cat_data.head()
+     
      
 
+#Missing columns in the test set are added
 
-#To account for missing values and columns
-trainservice = train['service'].tolist()
-testservice = test['service'].tolist()
+trainservice = df['service'].tolist()
+testservice = df_test['service'].tolist()
 difference = list(set(trainservice) - set(testservice))
 string = 'service_'
 difference = [string + x for x in difference]
 difference
 
 
-for col in difference:
-    test_cat_data[col] = 0
 
-print(train_cat_data.shape)    
-print(test_cat_data.shape)
+for col in difference:
+    testdf_cat_data[col] = 0
+
+print(df_cat_data.shape)    
+print(testdf_cat_data.shape)
+
+
+
+####Could not remove label changes and get code to work because of standardization and scaler
 
 #Add additional columns
 
-newTrain = train.join(train_cat_data)
-newTrain.drop('flag', axis=1, inplace=True)
-newTrain.drop('protocol_type', axis=1, inplace=True)
-newTrain.drop('service', axis=1, inplace=True)
+newdf=df.join(df_cat_data)
+newdf.drop('flag', axis=1, inplace=True)
+newdf.drop('protocol_type', axis=1, inplace=True)
+newdf.drop('service', axis=1, inplace=True)
 
-#test data
-newTest = test.join(test_cat_data)
-newTest.drop('flag', axis=1, inplace=True)
-newTest.drop('protocol_type', axis=1, inplace=True)
-newTest.drop('service', axis=1, inplace=True)
+# test data
+newdf_test=df_test.join(testdf_cat_data)
+newdf_test.drop('flag', axis=1, inplace=True)
+newdf_test.drop('protocol_type', axis=1, inplace=True)
+newdf_test.drop('service', axis=1, inplace=True)
 
-print(newTrain.shape)
-print(newTest.shape)
+print(newdf.shape)
+print(newdf_test.shape)
+
+
+
+
+
+# change the label column
+
 
 labelTrain=newTrain['label']
 labelTest=newTest['label']
 
-
-# change the label column
 pd.set_option('future.no_silent_downcasting', True)
 
-newlabelTrain=labelTrain.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1,
-                                  'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1,
-                                  'worm': 1, 'ipsweep' : 1,'nmap' : 1,'portsweep' : 1,'satan' : 1,'mscan' : 1,
-                                  'saint' : 1, 'ftp_write': 1,'guess_passwd': 1,'imap': 1,'multihop': 1,'phf': 1,
-                                  'spy': 1,'warezclient': 1,'warezmaster': 1,'sendmail': 1,'named': 1,'snmpgetattack': 1,
-                                  'snmpguess': 1,'xlock': 1,'xsnoop': 1,'httptunnel': 1,'buffer_overflow': 1,'loadmodule': 1,
-                                  'perl': 1,'rootkit': 1,'ps': 1,'sqlattack': 1,'xterm': 1 })
-newlabelTest=labelTest.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,
-                                'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,'ipsweep' : 1,
-                                'nmap' : 1,'portsweep' : 1,'satan' : 1,'mscan' : 1,'saint' : 1,'ftp_write': 1,'guess_passwd': 1,
-                                'imap': 1,'multihop': 1,'phf': 1,'spy': 1,'warezclient': 1,'warezmaster': 1,'sendmail': 1,
-                                'named': 1,'snmpgetattack': 1,'snmpguess': 1,'xlock': 1,'xsnoop': 1,'httptunnel': 1,
-                                'buffer_overflow': 1,'loadmodule': 1,'perl': 1,'rootkit': 1,'ps': 1,'sqlattack': 1,'xterm': 1})
-
+# change the label column
+newlabeldf=labeldf.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
+newlabeldf_test=labeldf_test.replace({ 'normal' : 0, 'neptune' : 1 ,'back': 1, 'land': 1, 'pod': 1, 'smurf': 1, 'teardrop': 1,'mailbomb': 1, 'apache2': 1, 'processtable': 1, 'udpstorm': 1, 'worm': 1,
+                           'ipsweep' : 2,'nmap' : 2,'portsweep' : 2,'satan' : 2,'mscan' : 2,'saint' : 2
+                           ,'ftp_write': 3,'guess_passwd': 3,'imap': 3,'multihop': 3,'phf': 3,'spy': 3,'warezclient': 3,'warezmaster': 3,'sendmail': 3,'named': 3,'snmpgetattack': 3,'snmpguess': 3,'xlock': 3,'xsnoop': 3,'httptunnel': 3,
+                           'buffer_overflow': 4,'loadmodule': 4,'perl': 4,'rootkit': 4,'ps': 4,'sqlattack': 4,'xterm': 4})
 
 # put the new label column back
-newTrain['label'] = newlabelTrain
-newTest['label'] = newlabelTest
+newdf['label'] = newlabeldf
+newdf_test['label'] = newlabeldf_test
 
- x_train = newTrain.drop(['duration','land','wrong_fragment','urgent','num_failed_logins','logged_in','num_compromised',
+x = newdf.drop(['duration','land','wrong_fragment','urgent','num_failed_logins','logged_in','num_compromised',
                 'num_file_creations','num_root','root_shell','su_attempted','num_shells','num_access_files',
                 'num_outbound_cmds','is_host_login','is_guest_login','flag_S2','flag_S3','flag_SH','srv_rerror_rate',
                 'service_csnet_ns','service_ctf','service_daytime','service_discard','service_domain','service_domain_u',
@@ -194,7 +214,7 @@ newTest['label'] = newlabelTest
                 'Protocol_type_udp','service_IRC','service_X11','service_Z39_50','service_auth','service_bgp','service_courier',
                 'service_http_8001','service_aol'], axis=1)
 
-x_test =newTest.drop(['duration','land','wrong_fragment','urgent','num_failed_logins','logged_in','num_compromised',
+x_test =newdf_test.drop(['duration','land','wrong_fragment','urgent','num_failed_logins','logged_in','num_compromised',
                          'num_file_creations','num_root','root_shell','su_attempted','num_shells','num_access_files',
                          'num_outbound_cmds','is_host_login','is_guest_login','flag_S2','flag_S3','flag_SH','srv_rerror_rate',
                          'service_csnet_ns','service_ctf','service_daytime','service_discard','service_domain','service_domain_u',
@@ -211,40 +231,45 @@ x_test =newTest.drop(['duration','land','wrong_fragment','urgent','num_failed_lo
                          'service_red_i','service_harvest','service_http_2784','dst_host_srv_rerror_rate','dst_host_rerror_rate',
                          'dst_host_srv_serror_rate','dst_host_serror_rate','dst_host_srv_diff_host_rate','Protocol_type_tcp',
                          'Protocol_type_udp','service_IRC','service_X11','service_Z39_50','service_auth','service_bgp','service_courier',
-                         'service_http_8001','service_aol'], axis=1)    
+                         'service_http_8001','service_aol'], axis=1)
+
 
 
 #standardization
 
 # Split dataframes into X & Y
 
-X_Train = x_train.drop('label', axis=1)
-Y_Train = newTrain.label
+X_Df = x.drop('label', axis=1)
+Y_Df = newdf.label
 
 # test set
-X_Test = x_test.drop('label', axis=1)
-Y_Test = newTest.label
+X_Df_test = x_test.drop('label', axis=1)
+Y_Df_test = newdf_test.label
 
-X_Train.shape
+
+X_Df.shape
+
 
 
 #columns saved for later use
 
-colNames_train = list(X_Train)
-colNames_test = list(X_Test)
+colNames = list(X_Df)
+colNames_test = list(X_Df_test)
 
-scaler1 = preprocessing.StandardScaler().fit(X_Train)
-X_Train=scaler1.transform(X_Train)
+scaler1 = preprocessing.StandardScaler().fit(X_Df)
+X_Df=scaler1.transform(X_Df)
 
 #test data
-scaler2 = preprocessing.StandardScaler().fit(X_Test)
-X_Test=scaler2.transform(X_Test)
+scaler2 = preprocessing.StandardScaler().fit(X_Df_test)
+X_Df_test=scaler2.transform(X_Df_test)
 
-X_Test = X_Test.astype('float32')
-Y_Test = Y_Test.astype('float32')
+X_Df_test = X_Df.astype('float32')
+Y_Df_test= Y_Df_test.astype('float32')
 
-y_binary = to_categorical(Y_Train)
-y_test_binary = to_categorical(Y_Test)
+
+y_binary = to_categorical(Y_Df)
+y_test_binary = to_categorical(Y_Df_test)
+
 
 
 
